676cbe071947f87560503324b946144629fa9a67
UncleKhab <cobrosoffice@gmail.com>
2020-10-20 00:13:26 +0300

    Almost done with the CSS

--- Day 1 - SignUp Form/app/signup/views.py
+++ Day 1 - SignUp Form/app/signup/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+from django.db import IntegrityError
 
 from .models import *
 
@@ -14,9 +15,39 @@ def index(request):
     context = {}
     return render(request, "signup/index.html", context)
 
+def login_view(request):
+    if request.method == "POST":
+        #Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        #Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return render(request, "signup/login.html", {
+                "message": "Logged in Successfully."
+            })
+        else:
+            context = {
+                "message": "Invalid username and/or password."
+            }
+            return render(request, "signup/login.html", context)
+    else:
+        context = {
+            "message": "wrong method"
+        }
+        return render(request, "signup/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 def register(request):
     if request.method == "POST":
-        username = request.POST["first-name"]
+        first_name = request.POST["first-name"]
+        last_name = request.POST["last-name"]
         email = request.POST["email"]
 
         password = request.POST["password"]
@@ -28,9 +59,14 @@ def register(request):
             })
 
         
-        user = User.objects.create_user(username, email, password)
-        user.save()
-        print("success")
+        try:
+            user = User.objects.create_user( email=email, first_name=first_name, last_name=last_name, password=password )
+            user.save()
+            print("success")
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Email Already in use"
+            })
         
         login(request, user)
         
@@ -38,4 +74,4 @@ def register(request):
             "message": "User Created"
         })
     else:
-        return render(request, "signup/register.html")
+        return HttpResponseRedirect(reverse("index"))
