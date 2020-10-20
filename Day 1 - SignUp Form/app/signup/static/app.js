fc120853702ea5206c7d1f3486c081a203919465
UncleKhab <cobrosoffice@gmail.com>
2020-10-21 00:37:26 +0300

    the form fades in and out

--- Day 1 - SignUp Form/app/signup/static/app.js
+++ Day 1 - SignUp Form/app/signup/static/app.js
@@ -16,20 +16,28 @@ da(".btn-sign").forEach(element => {
             d(".btn-sign-up").classList.add("active")
             d(".btn-sign-in").classList.remove("active")
             d(".btn-sign-in").setAttribute("data-switch", "sign-up")
-            d(".register").style.display = "block"
-            d(".log-in").style.display = "none"
-  
+            d(".log-in").style.animationPlayState = 'running'
         } 
         if(s === "sign-up"){
             d(".btn-sign-in").classList.add("active")
             d(".btn-sign-up").classList.remove("active")
             d(".btn-sign-in").setAttribute("data-switch", "sign-in")
-            d(".register").style.display = "none"
-            d(".log-in").style.display = "block"
+            d(".register").style.animationPlayState = 'running'
+
         } 
         console.log(s);
         setTimeout(function(){
             d(".btn-slider").style.animationPlayState = 'paused'
+            d(".register").style.animationPlayState = 'paused'
+            d(".log-in").style.animationPlayState = 'paused'
+            if (s === "sign-up"){
+                d(".register").style.display = "none"
+                d(".log-in").style.display = "block"
+            }
+            else{
+                d(".log-in").style.display = "none"
+                d(".register").style.display = "block"
+            }
         },1500)
     })
 })
